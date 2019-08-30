09734ff00f6b5328af4c7a02462a1c5fd651e9e2
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-08-30 08:35:54 -0400

    Update index.py

--- skybot/index.py
+++ skybot/index.py
@@ -11,36 +11,36 @@ The result of Skybot breaking free from RedBOT
 """
 
 
-class HelpFormat(DefaultHelpCommand):
-    def get_destination(self, no_pm: bool = False):
-        if no_pm:
-            return self.context.channel
-        else:
-            return self.context.author
+#class HelpFormat(DefaultHelpCommand):
+#    def get_destination(self, no_pm: bool = False):
+#        if no_pm:
+#            return self.context.channel
+#        else:
+#            return self.context.author
 
-    async def send_error_message(self, error):
-        destination = self.get_destination(no_pm=True)
-        await destination.send(error)
+  #  async def send_error_message(self, error):
+  #      destination = self.get_destination(no_pm=True)
+  #      await destination.send(error)
 
-    async def send_command_help(self, command):
-        self.add_command_formatting(command)
-        self.paginator.close_page()
-        await self.send_pages(no_pm=True)
+  #  async def send_command_help(self, command):
+ #       self.add_command_formatting(command)
+ #       self.paginator.close_page()
+ #       await self.send_pages(no_pm=True)
 
-    async def send_pages(self, no_pm: bool = False):
-        try:
-            if permissions.can_react(self.context):
-                await self.context.message.add_reaction(chr(0x2709))
-        except discord.Forbidden:
-            pass
+#    async def send_pages(self, no_pm: bool = False):
+   #     try:
+#            if permissions.can_react(self.context):
+ #               await self.context.message.add_reaction(chr(0x2709))
+  #      except discord.Forbidden:
+   #         pass
 
-        try:
-            destination = self.get_destination(no_pm=no_pm)
-            for page in self.paginator.pages:
-                await destination.send(page)
-        except discord.Forbidden:
-            destination = self.get_destination(no_pm=True)
-            await destination.send("Couldn't send help to you due to blocked DMs...")
+   #     try:
+   ##         destination = self.get_destination(no_pm=no_pm)
+    ##        for page in self.paginator.pages:
+    #            await destination.send(page)
+    #    except discord.Forbidden:
+   #         destination = self.get_destination(no_pm=True)
+    #        await destination.send("Couldn't send help to you due to blocked DMs...")
 
 print('Logged in as')
 print("Logging in...")
