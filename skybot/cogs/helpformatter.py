81e502064d869adac753db88ca9d8192ac273d12
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-02 09:09:31 -0400

    Update helpformatter.py

--- skybot/cogs/helpformatter.py
+++ skybot/cogs/helpformatter.py
@@ -19,19 +19,19 @@ async def help(self, ctx, * cog):
 ""
 try:
     if not cog:
-    halp = discord.Embed(title = ' Cog Listing and Uncatergorized Commands ',
-    description = ' Use `!help *cog*` to find out more about them!  \n (BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.) ')
-    cogs_desc = ' '
+        halp = discord.Embed(title = ' Cog Listing and Uncatergorized Commands ',
+        description = ' Use `!help *cog*` to find out more about them!  \n (BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.) ')
+        cogs_desc = ' '
     for x in self.bot.cogs:
-    cogs_desc += (' {} - {} '.format(x, self.bot.cogs[x].__doc__) + ' \n ')
-    halp.add_field(name = ' Cogs ', value = cogs_desc[0: len(cogs_desc) - 1], inline = False)
-    cmds_desc = ' '
-for y in self.bot.walk_commands():
-if not y.cog_name and not y.hidden:
-    cmds_desc += (' {} - {} '.format(y.name, y.help) + ' \n ')
-    halp.add_field(name = ' Uncatergorized Commands ', value = cmds_desc[0: len(cmds_desc) - 1], inline = False)
-    await ctx.message.add_reaction(emoji = ' ✉ ')
-    await ctx.message.author.send(' ', embed = halp)
+        cogs_desc += (' {} - {} '.format(x, self.bot.cogs[x].__doc__) + ' \n ')
+        halp.add_field(name = ' Cogs ', value = cogs_desc[0: len(cogs_desc) - 1], inline = False)
+        cmds_desc = ' '
+    for y in self.bot.walk_commands():
+    if not y.cog_name and not y.hidden:
+        cmds_desc += (' {} - {} '.format(y.name, y.help) + ' \n ')
+        halp.add_field(name = ' Uncatergorized Commands ', value = cmds_desc[0: len(cmds_desc) - 1], inline = False)
+        await ctx.message.add_reaction(emoji = ' ✉ ')
+        await ctx.message.author.send(' ', embed = halp)
 else :
 if len(cog) > 1:
     halp = discord.Embed(title = ' Error! ', description = ' That is way too many cogs! ', color = discord.Color.red())
