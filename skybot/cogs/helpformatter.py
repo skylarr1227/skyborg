659033b312031b00791881e7d1d0f20d46427bfe
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-02 08:44:03 -0400

    Update helpformatter.py

--- skybot/cogs/helpformatter.py
+++ skybot/cogs/helpformatter.py
@@ -1,59 +1,53 @@
 import discord
-from discord.ext import commands
+from discord.ext
+import commands
 
 class Help(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
+    self.bot = bot
 
-    @commands.command(name='help', aliases=['h'])
-    async def _help(self, ctx, *, command: str=None):
-        """Get help on a specified cog or command.
-        Don't put any arguments to get a list of available commands."""
-        pref = '```\n'
-        postf = f'Get info on a command group, category or just a command with @{self.bot.user.name}#{self.bot.user.discriminator} help <Category>/<Command>/<Command group> or *help <Category>/<Command>/<Command group>'
-        result = ''
-        postfix = '\n```'
-        paginator = commands.Paginator()
-        if not command:
-            li = [cog for cog in self.bot.cogs]
-            for smth in li:
-                if smth != 'Help':
-                    s = list(self.bot.cogs[smth].get_commands())
-                    if s:
-                        paginator.add_line(f"{s[0].cog_name}:")
-                        for c in s:
-                            if not c.hidden:
-                                paginator.add_line(f'    {c.name} - {c.short_doc}')
-            paginator.add_line(postf)
-            for page in paginator.pages:
-                await ctx.send(page)
-        else:
-            if command not in self.bot.all_commands:
-                if command not in self.bot.cogs:
-                    cmd = self.bot.get_command(command.replace('*', '').replace(self.bot.user.mention, ''))
-                    if cmd:
-                        paginator.add_line(f"{ctx.prefix.replace(self.bot.user.mention, f'@{self.bot.user.name}#{self.bot.user.discriminator} ')}{cmd.signature}\n\n    {cmd.help}")
-                        for page in paginator.pages:
-                            await ctx.send(page)
-                    else:
-                        result = 'That command/category/command group does not exist!'
-                        await ctx.send(result)
-                else:
-                    the_cog = list(command.get_commands())
-                    paginator.add_line(f"{the_cog[0].cog_name}:") 
-                    for cmd in the_cog:
-                        if not cmd.hidden:
-                            paginator.add_line(f'    {cmd.name} - {cmd.help}')
-                    paginator.add_line(postf)
-                    for page in paginator.pages:
-                        await ctx.send(page)
-            else:
-                cmd = self.bot.get_command(command.replace('*', '').replace(self.bot.user.mention, ''))
-                result += f"{ctx.prefix.replace(self.bot.user.mention, f'@{self.bot.user.name}#{self.bot.user.discriminator} ')}{cmd.signature}\n\nCog: {cmd.cog_name}\n\n    {cmd.help}"
-                await ctx.send(f"{pref}{result}{postfix}")
+@commands.command()
+commands.has_permissions(add_reactions = True, embed_links = True)
+async def help(self, ctx, * cog):
+    ""
+" Gets all cogs and commands of mine. "
+""
+try:
+if not cog:
+    halp = discord.Embed(title = ' Cog Listing and Uncatergorized Commands ',
+        description = ' Use `!help *cog*` to find out more about them!  \n (BTW, the Cog Name Must Be in Title Case, Just Like this Sentence.) ')
+cogs_desc = ' '
+for x in self.bot.cogs:
+    cogs_desc += (' {} - {} '.format(x, self.bot.cogs[x].__doc__) + ' \n ')
+halp.add_field(name = ' Cogs ', value = cogs_desc[0: len(cogs_desc) - 1], inline = False)
+cmds_desc = ' '
+for y in self.bot.walk_commands():
+    if not y.cog_name and not y.hidden:
+    cmds_desc += (' {} - {} '.format(y.name, y.help) + ' \n ')
+halp.add_field(name = ' Uncatergorized Commands ', value = cmds_desc[0: len(cmds_desc) - 1], inline = False)
+await ctx.message.add_reaction(emoji = ' ✉ ')
+await ctx.message.author.send(' ', embed = halp)
+else :
+    if len(cog) > 1:
+    halp = discord.Embed(title = ' Error! ', description = ' That is way too many cogs! ', color = discord.Color.red())
+await ctx.message.author.send(' ', embed = halp)
+else :
+    found = False
+for x in self.bot.cogs:
+    for y in cog:
+    if x == y:
+    halp = discord.Embed(title = cog[0] + ' Command Listing ', description = self.bot.cogs[cog[0]].__doc__)
+for c in self.bot.get_cog(y).get_commands():
+    if not c.hidden:
+    halp.add_field(name = c.name, value = c.help, inline = False)
+found = True
+if not found:
+    halp = discord.Embed(title = ' Error! ', description = ' How do you even use " ' + cog[0] + ' "? ', color = discord.Color.red())
+else :
+    await ctx.message.add_reaction(emoji = ' ✉ ')
+await ctx.message.author.send(' ', embed = halp)
+except:
+    pass
 
 def setup(bot):
     bot.add_cog(Help(bot))
-
-
-
