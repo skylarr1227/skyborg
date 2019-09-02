02c0552671e46890a7e5c1848c1face27d027ed9
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-02 09:12:44 -0400

    Update helpformatter.py

--- skybot/cogs/helpformatter.py
+++ skybot/cogs/helpformatter.py
@@ -28,31 +28,31 @@ try:
         cmds_desc = ' '
     for y in self.bot.walk_commands():
         if not y.cog_name and not y.hidden:
-        cmds_desc += (' {} - {} '.format(y.name, y.help) + ' \n ')
-        halp.add_field(name = ' Uncatergorized Commands ', value = cmds_desc[0: len(cmds_desc) - 1], inline = False)
-        await ctx.message.add_reaction(emoji = ' ✉ ')
+            cmds_desc += (' {} - {} '.format(y.name, y.help) + ' \n ')
+            halp.add_field(name = ' Uncatergorized Commands ', value = cmds_desc[0: len(cmds_desc) - 1], inline = False)
+            await ctx.message.add_reaction(emoji = ' ✉ ')
+            await ctx.message.author.send(' ', embed = halp)
+    else :
+    if len(cog) > 1:
+        halp = discord.Embed(title = ' Error! ', description = ' That is way too many cogs! ', color = discord.Color.red())
         await ctx.message.author.send(' ', embed = halp)
-else :
-if len(cog) > 1:
-    halp = discord.Embed(title = ' Error! ', description = ' That is way too many cogs! ', color = discord.Color.red())
-    await ctx.message.author.send(' ', embed = halp)
-else :
-    found = False
-for x in self.bot.cogs:
-for y in cog:
-if x == y:
-    halp = discord.Embed(title = cog[0] + ' Command Listing ', description = self.bot.cogs[cog[0]].__doc__)
-for c in self.bot.get_cog(y).get_commands():
-if not c.hidden:
-    halp.add_field(name = c.name, value = c.help, inline = False)
-    found = True
-if not found:
-    halp = discord.Embed(title = ' Error! ', description = ' How do you even use " ' + cog[0] + ' "? ', color = discord.Color.red())
-else :
-    await ctx.message.add_reaction(emoji = ' ✉ ')
-    await ctx.message.author.send(' ', embed = halp)
-except:
-pass
+    else :
+        found = False
+    for x in self.bot.cogs:
+    for y in cog:
+    if x == y:
+        halp = discord.Embed(title = cog[0] + ' Command Listing ', description = self.bot.cogs[cog[0]].__doc__)
+    for c in self.bot.get_cog(y).get_commands():
+    if not c.hidden:
+        halp.add_field(name = c.name, value = c.help, inline = False)
+        found = True
+    if not found:
+         halp = discord.Embed(title = ' Error! ', description = ' How do you even use " ' + cog[0] + ' "? ', color = discord.Color.red())
+    else :
+        await ctx.message.add_reaction(emoji = ' ✉ ')
+        wait ctx.message.author.send(' ', embed = halp)
+    except:
+    pass
 
-def setup(bot):
-    bot.add_cog(Help(bot))
+    def setup(bot):
+        bot.add_cog(Help(bot))
