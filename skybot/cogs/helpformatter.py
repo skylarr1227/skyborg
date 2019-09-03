e7bcd6514ccbaa88af9727b54dd5f76e00b0d735
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-03 08:55:32 -0400

    Update helpformatter.py

--- skybot/cogs/helpformatter.py
+++ skybot/cogs/helpformatter.py
@@ -30,13 +30,13 @@ try:
         if not y.cog_name and not y.hidden:
             cmds_desc += (' {} - {} '.format(y.name, y.help) + ' \n ')
             halp.add_field(name = ' Uncatergorized Commands ', value = cmds_desc[0: len(cmds_desc) - 1], inline = False)
-            await ctx.message.add_reaction(emoji = ' ✉ ')
+            await ctx.message.add_reaction(emoji = ' âœ‰ ')
             await ctx.message.author.send(' ', embed = halp)
     else:
         if len(cog) > 1:
             alp = discord.Embed(title = ' Error! ', description = ' That is way too many cogs! ', color = discord.Color.red())
             await ctx.message.author.send(' ', embed = halp)
-    else :
+    else:
         found = False
         for x in self.bot.cogs:
         for y in cog:
@@ -46,14 +46,14 @@ try:
         if not c.hidden:
         halp.add_field(name = c.name, value = c.help, inline = False)
         found = True
+        pass
     if not found:
          halp = discord.Embed(title = ' Error! ', description = ' How do you even use " ' + cog[0] + ' "? ', color = discord.Color.red())
-    else :
-        await ctx.message.add_reaction(emoji = ' ✉ ')
+    else:
+        await ctx.message.add_reaction(emoji = ' âœ‰ ')
         wait ctx.message.author.send(' ', embed = halp)
-   # except:
-  #  pass
-
+    except:
+    pass
 
     def setup(bot):
         bot.add_cog(Help(bot))
