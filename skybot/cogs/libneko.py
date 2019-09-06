f42e85442cbc3df982f473380b8262d150e195ab
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-05 21:25:39 -0400

    Create libneko.py

---
+++ skybot/cogs/libneko.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+from libneko import pag
+
+# Read the dummy text in.
+with open('dummy-text.txt') as fp:
+    dummy_text = fp.read()
+
+
+
+@bot.command()
+async def test(ctx):
+    """We will be coding in here in the next part."""
