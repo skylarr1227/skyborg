4f1356f08355447f904bcf23f9784fffe4a25747
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-09-05 21:38:27 -0400

    Update libneko.py

--- skybot/cogs/libneko.py
+++ skybot/cogs/libneko.py
@@ -10,7 +10,12 @@ class Libneko(commands.Cog)
 @bot.command()
 async def test(ctx):
     """We will be coding in here in the next part."""
+    nav = pag.StringNavigatorFactory(max_lines=10)
 
+    # Insert the dummy text into our factory.
+    nav += dummy_text
+
+nav.start(ctx)
 
 def setup(bot):
     bot.add_cog(Libneko(bot))
