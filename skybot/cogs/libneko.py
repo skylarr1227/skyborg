2f2c2168a5e7790e6138e5e8e13bc5d896cea2a0
Jay Kumar <suku260@gmail.com>
2019-09-05 23:11:57 -0500

    Update libneko.py
    input into def

--- skybot/cogs/libneko.py
+++ skybot/cogs/libneko.py
@@ -4,18 +4,18 @@ from discord.ext import commands
 from libneko import pag
 
 # Read the dummy text in.
-def read():
-    try:
-        with open('dummy-text.txt') as fp:
-            dummy_text = fp.read()
-    except:
-        await ctx.send("Could not read")
+
 class Libneko(commands.Cog):
     #def __init__(self)
     
     @commands.command()
     @read()
     async def test(ctx):
+        try:
+            with open('dummy-text.txt') as fp:
+                dummy_text = fp.read()
+        except:
+            await ctx.send("Could not read")
         """We will be coding in here in the next part."""
         nav = pag.StringNavigatorFactory(max_lines=10)
 
